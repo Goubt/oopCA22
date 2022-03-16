0f2b71710e71762d887c36a9297e4fe404374e0c
Goubt <thewierdone7@gmail.com>
2022-03-16 09:09:17 +0000

    Seperate Methods added to allow personal changes

--- java/src/ie/assignment/OOP.java
+++ java/src/ie/assignment/OOP.java
@@ -11,7 +11,6 @@ public class OOP extends PApplet
 	float rotation = 0;
 	float direction = 0;
 	Camera camera1;
-	
 
 	public void settings()
 	{
@@ -25,32 +24,25 @@ public class OOP extends PApplet
 							 width/2, height/2, 0,
 							 width/2, height/2, -width,
 							 0, 1, 0);
-		//camera1 = new Camera(this, 200, -250, 300);
 	}
-
-
 
 	public void draw()
 	{	
         background(0);
-		//float orbitRadius= 100;
 		camera1.feed();
+
 		translate(width/2, height/2, -width);
-		stroke(255,255,255);
-		noFill();
-		box(200);
+		start();
+
 		translate(width, 0, width);
-		stroke(255,0,0);
-		noFill();
-		box(200);
+		yaris();
+
 		translate(-width, 0, width);
-		stroke(0,255,0);
-		noFill();
-		box(200);
+		finn();
+
 		translate(-width, 0, -width);
-		stroke(0,0,255);
-		noFill();
-		box(200);
+		gooba();
+
 		if (direction > 0) {
 			RotateRight();
 			direction--;
@@ -62,22 +54,11 @@ public class OOP extends PApplet
 		
 	}
 	public void RotateRight(){
-		
 		camera1.look((float) (radians((float) (1))), 0);
-		
 	}
 
 	public void RotateLeft(){
-	
 		camera1.look(-(float) (radians((float) (1))), 0);
-	}
-
-	public void DrawSphere(){
-		strokeWeight(2);
-		stroke(200, 255, 255);
-		
-		translate(width/2, height/2, -100);
-        sphere(200);
 	}
 
 	public void keyPressed() {
@@ -94,6 +75,30 @@ public class OOP extends PApplet
 		if (keyCode == UP) {
 			camera(width/2, height/2, (height/2) / tan(PI/6), width/2, height/2, 0, 0, 1, 0);
 		}
+	}
+
+	public void start(){
+		stroke(255,255,255);
+		noFill();
+		box(200);
+	}
+
+	public void yaris(){
+		stroke(255,0,0);
+		noFill();
+		box(200);
+	}
+
+	public void finn(){
+		stroke(0,255,0);
+        noFill();
+        box(200);
+	}
+
+	public void gooba(){
+		stroke(0,0,255);
+		noFill();
+		box(200);
 	}
 
 }
